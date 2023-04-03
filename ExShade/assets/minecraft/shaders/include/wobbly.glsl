f62fc964f38da3e58c3bcbe4a6611d5de0fe4a38
ultraflame42 <34125174+ultraflame4@users.noreply.github.com>
2023-04-03 15:23:04 +0800

    added more wobbly to more blocjks

--- ExShade/assets/minecraft/shaders/include/wobbly.glsl
+++ ExShade/assets/minecraft/shaders/include/wobbly.glsl
@@ -6,12 +6,12 @@ vec3 wobble_position(vec3 position, float gametime) {
     float calmness = 4;
     vec3 pos = position;
 
-    float animX = (gametime * 2000 + pos.y + pos.z);
-    float animY = (gametime * 3000 + pos.x + pos.z);
-    float animZ = (gametime * 2500 + pos.x + pos.y);
+    float animX = (gametime * 3000 + pos.y + pos.z);
+    float animY = (gametime * 2500 + pos.x + pos.z);
+    float animZ = (gametime * 2000 + pos.x + pos.y);
 
     pos.x += sin(animX / calmness) * wobble_strength;
-    pos.y += sin(animY / calmness) * wobble_strength;
+    pos.y += cos(animY / calmness) * wobble_strength;
     pos.z += sin(animZ / calmness) * wobble_strength;
 
     return pos;
