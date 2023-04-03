e25e3d3b1b5d288c00fb3279e90764eadd8a608a
ultraflame42 <34125174+ultraflame4@users.noreply.github.com>
2023-04-03 11:12:58 +0800

    project setup

---
+++ README.md
@@ -0,0 +1,23 @@
+# ExShade
+Experimental resourcepack for experimenting with shaders in vanilla minecraft.
+
+
+## Development
+
+### Requirements.
+All requirements are in [requirements.txt](requirements.txt)
+
+`pip install -r requirements.txt` to install them.
+
+
+### watch.py
+This is the main file to run for development.
+
+watch.py is a script that watches for changes in the resourcepack 
+and copies them to the minecraft resourcepack folder.
+
+Edit the values in the file to match your setup.
+
+
+
+
---
+++ watch.py
@@ -0,0 +1,52 @@
+import os.path
+from pathlib import Path
+from dirsync import sync
+from watchdog.observers import Observer
+from watchdog.events import LoggingEventHandler
+
+# CONFIG START--------------------
+
+# Path to the resoucepack folder
+target_path = "ExShade"
+# Name of the resourcepack
+name="ExShade"
+# Path to the minecraft resourcepacks folder where the resourcepack will be copied to
+out_dir = "%appdata%/.minecraft/resourcepacks/"
+
+
+# CONFIG END--------------------
+
+
+def resolve_path(path: str) -> Path:
+    return Path(os.path.expandvars(path)).absolute()
+
+
+resolved_target = str(resolve_path(target_path))
+resolved_out = resolve_path(out_dir)/name
+
+
+class EventHandler(LoggingEventHandler):
+
+    def on_modified(self, event):
+        print("Change detected, copying...")
+        sync(
+            resolved_target,
+            resolved_out,
+            "sync",
+            verbose=True,
+            create=True,
+        )
+
+
+
+observer = Observer()
+observer.schedule(EventHandler(), resolved_target, recursive=True)
+print(f"Watching {resolved_target} for changes...")
+print("Will copy changes to:", resolved_out,"\n")
+observer.start()
+try:
+    while observer.isAlive():
+        observer.join(1)
+finally:
+    observer.stop()
+    observer.join()
